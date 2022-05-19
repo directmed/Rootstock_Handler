268751ed95c0f36e821f8f4ad44e1d8738d53d04
directmed <89492214+directmed@users.noreply.github.com>
2022-05-19 08:57:23 -0700

    main file.
    all code runs from this file.

---
+++ sources/wo_handler.py
@@ -0,0 +1,22 @@
+# Import all files in this folder
+from sources.handler import Handler
+from sources.gui_setup import GuiSetup
+from updater import Updater
+from sources.debugger_print import setup_files
+# from autocomplete import *
+# autocomplete_example(root)
+
+setup_files()
+main_updater = Updater()
+main_updater.start_updater()
+if main_updater.load_updater_github() is True:
+    main_updater.overwrite_files()
+
+
+main_handler = Handler()
+main_handler.start_tk()
+main_root = main_handler.root
+gui = GuiSetup(main_handler)
+gui.start_gui()
+main_root.mainloop()
+
